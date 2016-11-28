7624730ceadbba801b401ffc6258d2c33f6941ad
jprisel <a35392@alunos.isel.pt>
2016-11-28 21:13:56 +0000

    Emit Fix.

--- SettlerEmit/SettlerEmitter.cs
+++ SettlerEmit/SettlerEmitter.cs
@@ -18,38 +18,52 @@ namespace SettlerEmit
         MethodInfo callNew;
         MethodInfo callGetRandomString;
         MethodInfo callGetRandomInteger;
-        MethodInfo callToJson;
-        MethodInfo strBuilderAppend;
-        MethodInfo strBuilderToString;
-        LocalBuilder localStringBuilder;
+
+
         LocalBuilder localString;
         LocalBuilder paramObject;
         LocalBuilder lengthLocal;
-        
+
 
         public IFixture CreateAssembly(Type type)
         {
             string typeName = type.Name;
-            
+
             string ASM_NAME = AssemblyNamePrefix + typeName;
             string MOD_NAME = ASM_NAME;
             string TYP_NAME = ASM_NAME;
             string DLL_NAME = ASM_NAME + AssemblyFileExtension;
 
-            AssemblyBuilder asmBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(ASM_NAME), AssemblyBuilderAccess.RunAndSave);
+           
+            AssemblyBuilder asmBuilder = CreateAsm(ASM_NAME);
             ModuleBuilder modBuilder = asmBuilder.DefineDynamicModule(MOD_NAME, DLL_NAME);
-            TypeBuilder typeBuilder = modBuilder.DefineType(TYP_NAME);
-            typeBuilder.AddInterfaceImplementation(typeof(IFixture));
 
-            MethodBuilder FixtureMethodBuilder = typeBuilder.DefineMethod(
-                "Fixture",
+
+            TypeBuilder typeBuilder = modBuilder.DefineType(TYP_NAME,
+                TypeAttributes.Public,
+                typeof(object),
+                new Type[] { typeof(IFixture) });
+
+
+
+            MethodBuilder NewMethodBuilder = typeBuilder.DefineMethod(
+                "New",
                 MethodAttributes.Public |
                 MethodAttributes.Virtual |
                 MethodAttributes.ReuseSlot,
-                typeof(string),
-                new Type[1] { typeof(object) });
+                typeof(object),
+                new Type[] {});
 
-            ImplementFixtureMethod(FixtureMethodBuilder, type);
+            MethodBuilder FillMethodBuilder = typeBuilder.DefineMethod(
+                "Fill",
+                MethodAttributes.Public |
+                MethodAttributes.Virtual |
+                MethodAttributes.ReuseSlot,
+                typeof(object),
+                new Type[] { typeof(int)});
+
+            ImplementNewMethod(NewMethodBuilder, type);
+            ImplementFillMethod(FillMethodBuilder, type);
 
             Type FixtureType = typeBuilder.CreateType();
             asmBuilder.Save(DLL_NAME);
@@ -58,10 +72,25 @@ namespace SettlerEmit
             return (IFixture)Activator.CreateInstance(FixtureType);
         }
 
-        private void ImplementFixtureMethod(MethodBuilder fixtureMethodBuilder, Type type)
+        private void ImplementFillMethod(MethodBuilder newMethodBuilder, Type type)
+        {
+            il = newMethodBuilder.GetILGenerator();
+            string hello = "hello";
+            il.Emit(OpCodes.Ldstr, hello);
+            MethodInfo method = typeof(System.Console).GetMethod("WriteLine",
+                BindingFlags.Public | BindingFlags.Static, null, new Type[] { typeof(object) }, null);
+            il.Emit(OpCodes.Call, method);
+
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+        }
+
+        private void ImplementNewMethod(MethodBuilder fixtureMethodBuilder, Type type)
         {
             il = fixtureMethodBuilder.GetILGenerator();
-            ctor = typeof(StringBuilder).GetConstructor(new Type[] { });
+
+           
 
             callGetRandomString = typeof(Randomize).GetMethod("GetRandomString");
             //callGetRandomInteger = typeof(Randomize).GetMethod("GetRandomInteger");
@@ -76,124 +105,27 @@ namespace SettlerEmit
                 BindingFlags.Public | BindingFlags.Static, null, new Type[] { typeof(object) }, null);
             il.Emit(OpCodes.Call, method);
 
-            //if (type.IsArray)
-            //{
-            //    GetArrayValues();
-            //}
-            //else if (!type.IsPrimitive && type != typeof(string))
-            //{
-            //    int i = 0;
-            //    var properties = type.GetProperties();
-            //    AppendToStringBuilder("{");
-            //    foreach (var p in properties)
-            //    {
-            //        Type propType = p.PropertyType;
-
-            //        MethodInfo propertyGetMethod = p.GetGetMethod();
-
-            //        AppendToStringBuilder("\"" + p.Name + "\": ");
-            //        il.Emit(OpCodes.Ldloc_2);
-            //        il.Emit(OpCodes.Call, propertyGetMethod);//retorna o valor
-
-
-
-            //        if (propType.IsPrimitive || propType == typeof(string))
-            //        {
-            //            if (propType.IsValueType)
-            //            {
-            //                il.Emit(OpCodes.Box, propType);
-
-            //            }
-            //            else
-            //            {
-            //                lengthLocal = il.DeclareLocal(propType);
-            //                il.Emit(OpCodes.Castclass, typeof(object));
-            //                // il.Emit(OpCodes.Stloc_3);
-            //                // il.Emit(OpCodes.Ldloc_3);
-            //            }
-
-            //            il.Emit(OpCodes.Call, callGetPrimitiveValue);
-            //        }
-            //        else
-            //        {
-            //            il.Emit(OpCodes.Call, callToJson);
-            //        }
-            //        il.Emit(OpCodes.Stloc_1);
-            //        AppendToStringBuilder();
-            //        if (i < properties.Length - 1)
-            //            AppendToStringBuilder(",");
-            //        i++;
-
-            //    }
-            //    AppendToStringBuilder("}");
-            //}
-            //else
-            //{
-            //    EmitPrimitiveValue(type);
-            //}
-
-            //il.Emit(OpCodes.Ldloc_0);
-            //il.Emit(OpCodes.Call, strBuilderToString);
-            //il.Emit(OpCodes.Stloc_1);
-            //il.Emit(OpCodes.Ldloc_1);
+            il.Emit(OpCodes.Ldarg_0);
             il.Emit(OpCodes.Ret);
 
         }
 
-        private void AppendToStringBuilder()
+       
+
+
+
+
+
+
+
+        private static AssemblyBuilder CreateAsm(string name)
         {
-            il.Emit(OpCodes.Ldloc_0);
-            il.Emit(OpCodes.Ldloc_1);
-            il.Emit(OpCodes.Call, strBuilderAppend);
-            il.Emit(OpCodes.Pop);
-        }
-
-        private void AppendToStringBuilder(string str)
-        {
-            il.Emit(OpCodes.Ldloc_0);
-            il.Emit(OpCodes.Ldstr, str);
-            il.Emit(OpCodes.Call, strBuilderAppend);
-            il.Emit(OpCodes.Pop);
-        }
-
-        private void EmitPrimitiveValue(Type type)
-        {
-            il.Emit(OpCodes.Ldloc_2);
-
-            if (type.IsValueType)
-            {
-                il.Emit(OpCodes.Box, typeof(object));
-
-            }
-            else
-            {
-                lengthLocal = il.DeclareLocal(type);
-                il.Emit(OpCodes.Castclass, typeof(object));
-                // il.Emit(OpCodes.Stloc_3);
-                //  il.Emit(OpCodes.Ldloc_3);
-            }
-            //il.Emit(OpCodes.Call, callGetPrimitiveValue);
-            il.Emit(OpCodes.Stloc_1);
-            AppendToStringBuilder();
-        }
-
-
-
-
-
-
-        private void GetArrayValues()
-        {
-            // StringBuilder JSON = new StringBuilder("[");
-            // AppendToStringBuilder("[");
-
-            il.Emit(OpCodes.Ldloc_2);
-            //MethodInfo callArrayToJson = typeof(EmitterHelper).GetMethod("ArrayToJson");
-            //il.Emit(OpCodes.Call, callArrayToJson);
-            il.Emit(OpCodes.Stloc_1);
-            AppendToStringBuilder();
-            //  AppendToStringBuilder("]");
-
+            AssemblyName aName = new AssemblyName(name);
+            AssemblyBuilder ab =
+                AppDomain.CurrentDomain.DefineDynamicAssembly(
+                    aName,
+                    AssemblyBuilderAccess.RunAndSave);
+            return ab;
         }
     }
 }
---
+++ SettlerTest/Class1.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Settler.Test
+{
+    class Class1
+    {
+        //if (type.IsArray)
+        //{
+        //    GetArrayValues();
+        //}
+        //else if (!type.IsPrimitive && type != typeof(string))
+        //{
+        //    int i = 0;
+        //    var properties = type.GetProperties();
+        //    AppendToStringBuilder("{");
+        //    foreach (var p in properties)
+        //    {
+        //        Type propType = p.PropertyType;
+
+        //        MethodInfo propertyGetMethod = p.GetGetMethod();
+
+        //        AppendToStringBuilder("\"" + p.Name + "\": ");
+        //        il.Emit(OpCodes.Ldloc_2);
+        //        il.Emit(OpCodes.Call, propertyGetMethod);//retorna o valor
+
+
+
+        //        if (propType.IsPrimitive || propType == typeof(string))
+        //        {
+        //            if (propType.IsValueType)
+        //            {
+        //                il.Emit(OpCodes.Box, propType);
+
+        //            }
+        //            else
+        //            {
+        //                lengthLocal = il.DeclareLocal(propType);
+        //                il.Emit(OpCodes.Castclass, typeof(object));
+        //                // il.Emit(OpCodes.Stloc_3);
+        //                // il.Emit(OpCodes.Ldloc_3);
+        //            }
+
+        //            il.Emit(OpCodes.Call, callGetPrimitiveValue);
+        //        }
+        //        else
+        //        {
+        //            il.Emit(OpCodes.Call, callToJson);
+        //        }
+        //        il.Emit(OpCodes.Stloc_1);
+        //        AppendToStringBuilder();
+        //        if (i < properties.Length - 1)
+        //            AppendToStringBuilder(",");
+        //        i++;
+
+        //    }
+        //    AppendToStringBuilder("}");
+        //}
+        //else
+        //{
+        //    EmitPrimitiveValue(type);
+        //}
+
+        //il.Emit(OpCodes.Ldloc_0);
+        //il.Emit(OpCodes.Call, strBuilderToString);
+        //il.Emit(OpCodes.Stloc_1);
+        //il.Emit(OpCodes.Ldloc_1);
+    }
+}
