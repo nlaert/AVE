620c05eefff1d2acfd777672b845c4688278a311
nlaert <nlaert@gmail.com>
2016-12-29 23:32:17 +0000

    added Ignore method

--- AutoFixture/Fixture.cs
+++ AutoFixture/Fixture.cs
@@ -1,6 +1,8 @@
-﻿﻿using System;
+﻿﻿using Settler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +15,163 @@ namespace AutoFixture
     }
     public class Fixture<T> : IFixture
     {
+        private readonly Type klass;
+        private Object SingletonObject;
+        private bool IsSingleton;
+        private Dictionary<String, Object> Map = new Dictionary<string, object>();
+        private List<string> IgnoredProperties = new List<string>();
+        public Fixture()
+        {
+            klass = typeof(T);
+            IsSingleton = false;
+        }
+
+        public T New()
+        {
+            if (klass == null)
+                throw new NullReferenceException();
+            if (IsSingleton && SingletonObject != null)
+                return (T)SingletonObject;
+            T temp;
+            if (!klass.IsPrimitive && klass != typeof(string) && !klass.IsArray)
+            {
+                temp = (T)getInstance(klass);
+                foreach (PropertyInfo prop in klass.GetProperties())
+                {
+                    Object value;
+                    if (Map.TryGetValue(prop.Name, out value))
+                    {
+                        if (typeof(IFixture).IsAssignableFrom(value.GetType()))
+                        {
+                            var x = value.GetType().GetMethod("New").Invoke(value, null);
+                            prop.SetValue(temp, x);
+                        }
+                        else
+                            prop.SetValue(temp, value);
+                    }
+                    else
+                        prop.SetValue(temp, AutoFixture.For(prop.PropertyType).New());
+                }
+
+                foreach (FieldInfo field in klass.GetFields())
+                {
+                    Object value;
+                    if (Map.TryGetValue(field.Name, out value))
+                        field.SetValue(temp, value);
+                    else
+                        field.SetValue(temp, AutoFixture.For(field.FieldType).New());
+                }
+                SingletonObject = temp;
+                return temp;
+            }
+            if (klass.IsArray)
+            {
+                return (T)AutoFixture.For(klass.GetElementType()).Fill(Randomize.GetRandomInteger());
+            }
+            return (T)FillPrimitive();
+
+        }
+
+        Object IFixture.New()
+        {
+            return this.New();
+        }
+
+        Object IFixture.Fill(int size)
+        {
+            return this.Fill(size);
+        }
+
+        public Fixture<T> Singleton()
+        {
+            IsSingleton = true;
+            return this;
+        }
+
+        public Fixture<T> Member(string name, params object[] pool)
+        {
+            IEnumerable<PropertyInfo> pi = klass.GetProperties().Where(p => p.Name.Equals(name));
+            if (pi.Count() == 0)
+                throw new InvalidOperationException();
+            var index = Randomize.GetRandomInteger(pool.Length);
+            /*
+            if (typeof(IFixture).IsAssignableFrom(pool[index].GetType()))
+            {
+                var x = pool[index].GetType().GetMethod("New").Invoke(pool[index], null);
+                Map.Add(name, x);
+                //falta ir buscar o valor do singleton
+            }
+            else
+
+                */
+            Map.Add(name, pool[index]);
+
+
+            return this;
+        }
+
+        public T[] Fill(int size)
+        {
+            T[] res = new T[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                res[i] = New();
+            }
+            return res;
+        }
+
+        public object FillPrimitive()
+        {
+            if (klass == typeof(string))
+                return Randomize.GetRandomString();
+            else
+                return Randomize.GetRandomInteger();
+        }
+
+        public Fixture<T> Ignore(string ToBeIgnored)
+        {
+            if(!IgnoredProperties.Contains(ToBeIgnored))
+                IgnoredProperties.Add(ToBeIgnored);
+            return this;
+        }
+
+        public static object getInstance(Type t)
+        {
+            ConstructorInfo ci = getSmallestConstructor(t);
+            ParameterInfo[] pi = ci.GetParameters();
+            object[] parameters = new object[pi.Length];
+
+            for (int i = 0; i < pi.Length; i++)
+            {
+                ParameterInfo parameterInfo = pi[i];
+                ConstructorInfo cType = getSmallestConstructor(parameterInfo.ParameterType);
+                if (cType == null || cType.GetParameters().Length == 0)
+                    parameters[i] = Activator.CreateInstance(parameterInfo.ParameterType);
+                else
+                {
+                    parameters[i] = getInstance(parameterInfo.ParameterType);
+                }
+            }
+            return ci.Invoke(parameters);
+        }
+
+        private static ConstructorInfo getSmallestConstructor(Type classType)
+        {
+            ConstructorInfo[] constructors = classType.GetConstructors();
+            if (constructors.Length == 0) return null;
+            ConstructorInfo smallestCi = constructors[0];
+            int smaller = constructors[0].GetParameters().Length;
+            foreach (ConstructorInfo ci in constructors)
+            {
+                int dimension = ci.GetParameters().Length;
+                if (dimension <= smaller)
+                {
+                    smallestCi = ci;
+                    smaller = dimension;
+                }
+            }
+            return smallestCi;
+        }
     }
 }
